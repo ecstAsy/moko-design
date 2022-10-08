d1fc95a4eb5ba1dd75fab49e8a88db26c8375981
hexiaoqiang <sky_hmn@163.com>
2022-10-08 16:14:56 +0800

    feat: add logo.svg

--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,26 @@
-# Hello VitePress
+---
+layout: home
+title: moko-disign
+editLink: true
+
+titleTemplate: A Vue3 UI Framework
+
+hero:
+  name: Moko Design
+  tagline: Vue3 Moko UI Framework
+  actions:
+    - theme: brand
+      text: 快速上手 →
+      link: /zh-CN/guide/installation
+    - theme: alt
+      text: GitHub
+      link: https://github.com/wei-design/web-vue
+
+features:
+  - title: 📦 Vite
+    details: Vite打包
+  - title: ⚡️ Vue3
+    details: Vue3 + TypeScript组件
+  - title: 📃 Vitepress
+    details: Vitepress搭建文档库
+---
--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="/logo.svg" class="logo" alt="Vite logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
   <m-button/>
 </template>
 
@@ -25,8 +20,5 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
