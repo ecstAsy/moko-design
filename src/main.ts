030b28f1f3e2c2c91653d1ae99c13ead28d6dc93
hexiaoqiang <sky_hmn@163.com>
2022-10-10 13:49:32 +0800

    feat:add element-plus/icon

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import MokoDesign from '../packages/index';
+import { createApp } from "vue";
+import * as Icons from "@element-plus/icons-vue";
+import "./style.css";
+import App from "./App.vue";
+import MokoDesign from "../packages/index";
 
 const app = createApp(App);
-
-app.use(MokoDesign).mount('#app')
+Object.keys(Icons).forEach((icon) => {
+  app.component(icon, Icons[icon as keyof typeof Icons]);
+});
+app.use(MokoDesign).mount("#app");
