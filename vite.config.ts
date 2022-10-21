62684fe601ec0b1d5fdee3d0d6c0e9a67f54afd6
hexiaoqiang <sky_hmn@163.com>
2022-10-21 10:39:24 +0800

    chore:add @types/node

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueSetupExtend from "vite-plugin-vue-setup-extend";
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import { defineConfig } from 'vite';
+import path from 'path';
+import vue from '@vitejs/plugin-vue';
+import vueSetupExtend from 'vite-plugin-vue-setup-extend';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,8 +18,13 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
   ],
+  resolve: {
+    alias: [
+      { find: /^@\//, replacement: `${path.resolve(__dirname, './src')}/` },
+    ],
+  },
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: 8888,
     open: true,
   },
