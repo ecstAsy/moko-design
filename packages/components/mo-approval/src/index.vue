af3238287693da19add6789b480d186dd8951f87
hexiaoqiang <sky_hmn@163.com>
2022-11-02 13:54:14 +0800

    perf:MokoApproval组件

--- packages/components/mo-approval/src/index.vue
+++ packages/components/mo-approval/src/index.vue
@@ -1,6 +1,14 @@
 <!--
  * @Author: ecstAsy
  * @Date: 2022-11-01 11:41:31
- * @LastEditTime: 2022-11-01 13:06:10
+ * @LastEditTime: 2022-11-01 13:18:01
  * @LastEditors: ecstAsy
 -->
+<template>
+  <div class="moko-approval">
+    <div class="moko-approval-content">
+      <div id="stencil" calss="MStencil" />
+      <div ref="MGraph" calss="MGraph" />
+    </div>
+  </div>
+</template>
