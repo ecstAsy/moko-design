c70f82b135b2b10e06f353a47ace38b60e740a77
hexiaoqiang <sky_hmn@163.com>
2022-12-15 11:10:56 +0800

    chore:添加@antv/x6依赖

--- packages/components/mo-approval/src/index.vue
+++ packages/components/mo-approval/src/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: ecstAsy
  * @Date: 2022-11-01 11:41:31
- * @LastEditTime: 2022-11-01 13:18:01
+ * @LastEditTime: 2022-12-15 11:09:33
  * @LastEditors: ecstAsy
 -->
 <template>
@@ -12,3 +12,5 @@
     </div>
   </div>
 </template>
+
+<script setup lang="ts"></script>
